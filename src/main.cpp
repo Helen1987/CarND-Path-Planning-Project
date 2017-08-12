f014aa81a8866187446d2bbd66e08179520f2e92
Alena Kastsiukavets <kastsiukavets.alena@gmail.com>
2017-08-12 19:14:08 +0300

    feat: last implementation from Q&A

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,7 @@ int main() {
   }
 
   int lane = 1;
-  double ref_vel = 49.5;//mps
+  double ref_vel = 0.0;//mps
 
   h.onMessage([&ref_vel, &lane, &map_waypoints_x, &map_waypoints_y, &map_waypoints_s, &map_waypoints_dx, &map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
     uWS::OpCode opCode) {
@@ -113,7 +113,42 @@ int main() {
           json msgJson;
 
           int prev_size = previous_path_x.size();
-          cout << "prev size : " << prev_size << endl;
+          
+          if (prev_size > 0) {
+            car_s = end_path_s;
+          }
+
+          bool too_close = false;
+
+          // find ref_v to use
+          for (int i = 0; i < sensor_fusion.size(); ++i) {
+            // car is in my lane
+            float d = sensor_fusion[i][6];
+            if (d < (2 + 4 * lane + 2) && d >(2 + 4 * lane - 2)) {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx*vx + vy*vy);
+              double check_car_s = sensor_fusion[i][5];
+
+              check_car_s += (double)prev_size*.02*check_speed; // if using previous points can project s value out
+              // check s values greater than mine and s gap
+              if ((check_car_s > car_s) && (check_car_s - car_s) < 30) {
+                too_close = true;
+                if (lane > 0) {
+                  lane = 0;
+                }
+              }
+
+            }
+          }
+
+          if (too_close) {
+            ref_vel -= .224;
+          }
+          else if (ref_vel < 49.5) {
+            ref_vel += .224;
+          }
+
 
           // Create a list of widly spaced waypoints (x,y), evenly spaced at 30 m
           // Later we will interpolate these waypoints with a spline and fill it in with more points tha control speed
